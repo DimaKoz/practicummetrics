6109c0d2fbf9d3b5852e4eb8553e7e3af16273f3
kozhevin <kozhevin.dima@gmail.com>
2023-04-06 22:46:09 +0300

    resty

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/DimaKoz/practicummetrics/internal/server/handler"
 	"github.com/DimaKoz/practicummetrics/internal/server/middleware"
-	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
-
-	//gin  emulated
-	r := gin.Default()
-	fmt.Println(r)
 
 	mux := http.NewServeMux()
 	mux.Handle(`/update/`, middleware.MethodChecker(http.HandlerFunc(handler.UpdateHandler)))
--- internal/agent/send/sender.go
+++ internal/agent/send/sender.go
@@ -3,7 +3,7 @@ package send
 import (
 	"fmt"
 	"github.com/DimaKoz/practicummetrics/internal/common/model"
-	"net/http"
+	"github.com/go-resty/resty/v2"
 	"os"
 )
 
@@ -11,18 +11,13 @@ import (
 func ParcelsSend(metrics []model.MetricUnit) {
 	for _, unit := range metrics {
 		url := "http://localhost:8080/update/" + prepPathByMetric(unit)
-		req, err := http.NewRequest(http.MethodPost, url, nil)
+		client := resty.New()
+		_, err := client.R().Post(url)
 		if err != nil {
 			fmt.Printf("client: could not create request: %s\n", err)
 			os.Exit(1)
 		}
 
-		res, err := http.DefaultClient.Do(req)
-		if err != nil {
-			fmt.Printf("client: error making http request: %s\n", err)
-			os.Exit(1)
-		}
-		res.Body.Close()
 	}
 
 }
