50ccab6ad2285e402f8eb766bb4cfed71910463e
kozhevin <kozhevin.dima@gmail.com>
2023-04-07 00:18:11 +0300

    useless comments have been removed

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,11 +11,6 @@ func main() {
 	e.POST("/update/*", handler.UpdateHandler)
 	e.GET("/value/*", handler.ValueHandler)
 	e.GET("/", handler.RootHandler)
-	//mux := http.NewServeMux()
-
-	//mux.Handle(`/update/`, middleware.MethodChecker(http.HandlerFunc(handler.UpdateHandler)))
-	//mux.Handle(`/value/`, http.HandlerFunc(handler.ValueHandler))
-	//mux.HandleFunc(`/`, handler.RootHandler)
 
 	err := e.Start(":8080")
 	if err != nil {
