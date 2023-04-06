778e3e473183a768b3646a86405e98990469f451
kozhevin <kozhevin.dima@gmail.com>
2023-04-06 18:49:42 +0300

    an html page for '/'

--- internal/server/handler/roothandler.go
+++ internal/server/handler/roothandler.go
@@ -1,8 +1,26 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"github.com/DimaKoz/practicummetrics/internal/common/repository"
+	"net/http"
+)
 
 // RootHandler handles `/`
 func RootHandler(res http.ResponseWriter, req *http.Request) {
-	res.WriteHeader(http.StatusNotFound)
+	if req.URL.Path != "/" {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+	metrics := repository.GetMetricsMemStorage()
+	var body = ""
+	for i, m := range metrics {
+		if i != 0 {
+			body += "<br></br>"
+		}
+		body += m.Name + "," + m.Value
+	}
+
+	fmt.Fprintf(res, "<h1>%s</h1><div>%s</div>", "Metrics:", body)
+
 }
